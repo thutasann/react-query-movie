dba754ff49d30af8407ed7e1b7090a28787557bb
thutasann <thutasann@gmail.com>
2022-12-20 22:27:59 +0630

    Components

---
+++ components/Grid.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    title: string;
+    children: React.ReactNode;
+    className?: string;
+}
+
+function Grid({ title, children, className } : Props) {
+    return (
+        <div className={className}>
+            <h2 className='text-xl font-bold pb-4'>{title}</h2>
+            <div className='grid grid-cols-auto-fill gap-8'>{children}</div>
+        </div>
+    )
+}
+
+export default Grid
