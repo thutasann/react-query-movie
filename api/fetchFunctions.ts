a94be9af78e710f876cc62de7863440c5d261603
thutasann <thutasann@gmail.com>
2022-12-20 23:53:43 +0630

    server side api route

---
+++ api/fetchFunctions.ts
@@ -0,0 +1,22 @@
+import { Movies } from "./types";
+
+/**
+ * Basic Fetch
+ */
+export const basicFetch = async <returnType>(endpoint: string): Promise<returnType> =>{
+    const response = await fetch(endpoint);
+
+    if(!response.ok) throw new Error("Error!");
+
+    const data = await response.json();
+
+    return data;
+};
+
+
+/** 
+ * Fetch Fucntions
+ */
+export const fetchMovies = async(search = '', page = 1) : Promise<Movies> => {
+    return await basicFetch<Movies>(`/api/movies?search=${search}&page=${page}`);
+};
